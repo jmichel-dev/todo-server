af0f8984d55591bf53cadd5ea46553e3a59bb3b3
Jean Santos <jeaanmichel@protonmail.com>
2022-03-27 12:08:17 -0300

    password hashing for user create account

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -1,4 +1,5 @@
 import AppError from '@shared/errors/AppError';
+import { hash } from 'bcryptjs';
 import { getCustomRepository } from 'typeorm';
 import User from '../typeorm/entities/User';
 import UsersRepository from '../typeorm/repositories/UserRepository';
@@ -20,15 +21,17 @@ class CreateUserService {
     const usersRepository = getCustomRepository(UsersRepository);
     const userEmailExisis = await usersRepository.findByEmail(email);
 
-    if (!userEmailExisis) {
+    if (userEmailExisis) {
       throw new AppError('Email address already exists');
     }
+
+    const hashedPassword = await hash(password, 8);
 
     const user = usersRepository.create({
       first_name,
       last_name,
       email,
-      password,
+      password: hashedPassword,
     });
 
     await usersRepository.save(user);
