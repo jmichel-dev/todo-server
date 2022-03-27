f53c81a89f743be7ede94ccf2142b2bcf048616d
Jean Santos <jeaanmichel@protonmail.com>
2022-03-27 12:06:19 -0300

    defined users routes

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -6,12 +6,14 @@ import cors from 'cors';
 
 import '@shared/typeorm';
 import AppError from '@shared/errors/AppError';
+import routes from './routes';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+app.use(routes);
 app.use(errors());
 
 app.use(
