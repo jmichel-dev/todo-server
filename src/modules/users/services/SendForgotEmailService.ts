86e24f2926132d099861cfb7bff8934c364ba1f8
Jean Santos <jeaanmichel@protonmail.com>
2022-04-03 15:17:45 -0300

    configured send mail for forgot password email

--- src/modules/users/services/SendForgotEmailService.ts
+++ src/modules/users/services/SendForgotEmailService.ts
@@ -1,4 +1,6 @@
+import EtherealMail from '@config/mail/EtherealMail';
 import AppError from '@shared/errors/AppError';
+import path from 'path';
 import { getCustomRepository } from 'typeorm';
 import UsersRepository from '../typeorm/repositories/UserRepository';
 import UserTokenRepository from '../typeorm/repositories/UserTokenRepository';
@@ -20,8 +22,26 @@ export default class SendForgotEmailService {
 
     const { token } = await userTokensRepository.generate(user.id);
 
-    console.log(token);
+    const forgotPasswordTemplate = path.resolve(
+      __dirname,
+      '..',
+      'views',
+      'forgot_password.hbs',
+    );
 
-    // TODO - send email notification
+    await EtherealMail.sendMail({
+      to: {
+        name: user.first_name,
+        email: user.email,
+      },
+      subject: '[API Vendas] Recuperação de senha',
+      templateData: {
+        file: forgotPasswordTemplate,
+        variables: {
+          name: user.first_name,
+          link: `http://localhost:3000/reset_password?token=${token}`,
+        },
+      },
+    });
   }
 }
