d5230c10b409b41727a80c4a4db7e8a5798f2bc2
Jean Santos <jeaanmichel@protonmail.com>
2022-04-09 14:51:18 -0300

    added routes for tasks management

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import sessionRouter from '@modules/users/routes/sessions.routes';
 import userRouter from '@modules/users/routes/users.routes';
 import passwordRouter from '@modules/users/routes/password.routes';
+import taskRouter from '@modules/tasks/routes/tasks.routes';
 
 const routes = Router();
 
 routes.use('/users', userRouter);
 routes.use('/auth', sessionRouter);
 routes.use('/password', passwordRouter);
+routes.use('/tasks', taskRouter);
 
 export default routes;
